0e21e8910d2176c1d6f77bdc28bdbc0772e72989
Michael Dul <michaldul@Michaels-MacBook-Pro.local>
2018-04-10 23:53:33 +0200

    Version 2

--- app-window-shopper/Controller/MainVC.swift
+++ app-window-shopper/Controller/MainVC.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    @IBOutlet weak var wageTxt: CurrencyTestField!
+    @IBOutlet weak var priceTxt: CurrencyTestField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 50))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
 
+    @objc func calculate() {
+        print("We got here")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- app-window-shopper/View/CurrencyTestField.swift
+++ app-window-shopper/View/CurrencyTestField.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
+
+@IBDesignable
 class CurrencyTestField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customisedView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customisedView()
+    }
+
+    func customisedView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2485552226)
         layer.cornerRadius = 5.0
         textAlignment = .center
-        if placeholder == nil {
-            placeholder = " "
-        }
+        //        if placeholder == nil {
+        //            placeholder = " "
+        //        }
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
@@ -25,5 +35,4 @@ class CurrencyTestField: UITextField {
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-
 }
